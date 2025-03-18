8cf039f8bfab2f4a1119ccc3ef4dd088c87c2ee5
TheoQueiroz <120662161+TheoQueiroz@users.noreply.github.com>
2025-03-18 16:10:30 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function updateDashboard() {
     var iframe = document.getElementById('dashboard-iframe');
     if (window.innerWidth <= 768) { // Tamanho típico de um dispositivo móvel
-        iframe.src = 'https://lookerstudio.google.com/embed/reporting/f9cc18f8-c1a0-4cdf-8f07-3f3c316b7741/page/BUF3D';
+        iframe.src = 'https://lookerstudio.google.com/embed/reporting/7c015a7a-5ca7-41c9-8616-eab20e920360/page/fslCF';
     } else {
-        iframe.src = 'https://lookerstudio.google.com/embed/reporting/1c710392-f367-4239-8298-23b5e4f8bb8e/page/BUF3D';
+        iframe.src = 'https://lookerstudio.google.com/embed/reporting/7c015a7a-5ca7-41c9-8616-eab20e920360/page/fslCF';
     }
 }
 
@@ -12,3 +12,5 @@ updateDashboard();
 
 // Atualiza o dashboard quando o tamanho da janela é redimensionado
 window.addEventListener('resize', updateDashboard);
+
+//https://lookerstudio.google.com/embed/reporting/1c710392-f367-4239-8298-23b5e4f8bb8e/page/BUF3D Antigo
