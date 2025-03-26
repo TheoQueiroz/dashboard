41186e1c4ef0511861c57a8daf321e2865c498a1
TheoQueiroz <120662161+TheoQueiroz@users.noreply.github.com>
2025-03-26 09:52:45 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function updateDashboard() {
     var iframe = document.getElementById('dashboard-iframe');
     if (window.innerWidth <= 768) { // Tamanho típico de um dispositivo móvel
-        iframe.src = 'https://lookerstudio.google.com/embed/reporting/7c015a7a-5ca7-41c9-8616-eab20e920360/page/fslCF';
+        iframe.src = 'https://lookerstudio.google.com/embed/reporting/9f6831cc-bcd9-42f0-832d-eabc8abcfef8/page/fslCF';
     } else {
         iframe.src = 'https://lookerstudio.google.com/embed/reporting/7c015a7a-5ca7-41c9-8616-eab20e920360/page/fslCF';
     }
